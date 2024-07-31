96803769a9d2766ad642737aaf5e95891e560f1b
MoysisPap <144357526+MoysisPap@users.noreply.github.com>
2024-07-31 11:24:30 +0200

    Contact-form update

--- src/common/ThemeContext.jsx
+++ src/common/ThemeContext.jsx
@@ -6,7 +6,7 @@ export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(
-    () => localStorage.getItem('theme') || 'light'
+    () => localStorage.getItem('theme') || 'dark'
   );
 
   useEffect(() => {
--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form action="https://formspree.io/f/xeojgawz" method="POST">
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
@@ -37,7 +37,8 @@ function Contact() {
             name="message"
             id="message"
             placeholder="Message"
-            required></textarea>
+            required
+          ></textarea>
         </div>
         <input className="hover btn" type="submit" value="Submit" />
       </form>
